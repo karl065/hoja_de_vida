016158e07acf2cbfc0232aec77468260785ab90f
karl065 <karlos065@hotmail.com>
2023-04-17 16:50:00 -0500

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['public/index.html', './src/**/*.js'],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
